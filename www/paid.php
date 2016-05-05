3b7f1045d61fbda85fc5458b50f7ecb9edbb452d
Lionel Martin <lm@purebilling.com>
2016-05-05 18:22:34 +0200

    demo with material skin

--- www/paid.php
+++ www/paid.php
@@ -1,13 +1,22 @@
-<pre>
+<html>
+<head>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/dracula.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="skinned/assets/paid.css">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js"></script>
+
+</head>
+<body>
+<div class="container">
 <?php
 
 include_once('helper.php');
 
 if (!empty($_POST)) {
-    echo "POST DATA RECEIVED:", PHP_EOL;
+
     $postData = $_POST;
-    var_dump($postData);
-    echo "<br><br>";
+    $data1 = json_encode($postData, JSON_PRETTY_PRINT);
 
     $billingId = $postData["kr_billing_transaction"];
 
@@ -15,11 +24,29 @@ if (!empty($_POST)) {
     $request["id"] = $billingId;
     $result = getJson('Charge/Get', $request);
 
-    echo "Charge/Get WEBSERVICE ANSWER:", PHP_EOL;
-    echo "<pre>".var_dump($result['answer'])."</pre>";
+    if ($result['answer']['status'] == 'paid') {
+        print "<h2>Transaction has been paid !!</h2>";
+    }
+
+    echo "<h2>post data received:</h2>", PHP_EOL;
+    echo "<pre><code class=\"json\">$data1</code></pre>";
+
+
+
+    echo "<h2>Charge/Get web-service answer:</h2>", PHP_EOL;
+    $data = json_encode($result, JSON_PRETTY_PRINT);
+    echo "<pre><code class=\"json\">$data</code></pre>";
 } else {
     echo "No post data received";
 }
 
 ?>
-</pre>
+</div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('pre code').each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
+    });
+</script>
+</body></html>
---
+++ www/skinned/index.php
@@ -0,0 +1,103 @@
+<?php
+include_once('../helper.php');
+
+$request = [];
+$request["amount"] = 990;
+$request["currency"] = 'EUR';
+$result = getJson('Charge/Capture', $request);
+
+
+$formToken = $result['answer']['formToken'];
+
+$comment =  "New form token genrated: " . $formToken. "<br><br>";
+
+?>
+<!doctype html>
+<html>
+<head>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,100,300">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/animatecss/3.5.1/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/material.css">
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+</head>
+
+<body>
+
+<p><?php echo $comment;?></p>
+
+<div id="wrapper" class="card">
+    <div id="header" class="valign-wrapper">
+        <h2>
+            Demo
+        </h2>
+
+    </div>
+
+    <div id="form" class="kr-embedded container">
+
+        <!-- Card number -->
+        <div class="row">
+            <div class="col s3 icon-container"><i class="zmdi zmdi-card"></i></div>
+            <div class="col s7 kr-pan" kr-style='{"input":{"border":"0px","border-bottom":"1px solid #ccc"}}'>
+
+            </div>
+            <div class="col s2">&nbsp;</div>
+        </div>
+
+        <!-- Expiration month -->
+        <div class="row">
+            <div class="col s3 icon-container"><i class="zmdi zmdi-calendar"></i></div>
+            <div class="col s7 kr-expiry-month" kr-style='{"input":{"border":"0px","border-bottom":"1px solid #ccc"}}'>
+
+            </div>
+            <div class="col s2">&nbsp;</div>
+        </div>
+
+        <!-- Expiration year -->
+        <div class="row">
+            <div class="col s3 icon-container"><i class="zmdi zmdi-calendar"></i></div>
+            <div class="col s7 kr-expiry-year" kr-style='{"input":{"border":"0px","border-bottom":"1px solid #ccc"}}'>
+
+            </div>
+            <div class="col s2">&nbsp;</div>
+        </div>
+
+        <!-- CVV -->
+        <div class="row">
+            <div class="col s3 icon-container"><i class="zmdi zmdi-shield-security"></i></div>
+            <div class="col s7 kr-security-code" kr-style='{"input":{"border":"0px","border-bottom":"1px solid #ccc"}}'>
+
+            </div>
+            <div class="col s2">&nbsp;</div>
+        </div>
+
+        <div class="row">
+            <div class="col s3">&nbsp;</div>
+            <div class="col s6 valign-wrapper center-align">
+                <button type="submit" class="btn kr-payment-button waves-effect waves-light">Pay</button>
+            </div>
+            <div class="col s3">&nbsp;</div>
+
+        </div>
+
+    </div>
+</div>
+
+
+<script src="https://krypton.purebilling.io/V3/stable/kr.debug.js?formToken=<?php echo $formToken?>"
+        kr-public-key="demo:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5"
+        kr-style='{"wrapper":{"font-family":"Trebuchet MS"}}'
+        kr-post-url="/paid.php"
+        kr-language="es"></script>
+
+<script type="text/javascript" src="assets/material.js"></script>
+
+</body>
+
+</html>
