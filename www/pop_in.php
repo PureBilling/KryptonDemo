532afc32030dd6bc9b583676d9dd422d75e8482d
Lionel Martin <lm@purebilling.com>
2016-06-23 14:48:33 +0200

    rename kr-company-name to kr-shop-name

--- www/pop_in.php
+++ www/pop_in.php
@@ -15,7 +15,7 @@ echo "New form token genrated: " . $formToken . "<br><br>";
 
 <script src="https://krypton.purebilling.io/V3/stable/kr.debug.js?formToken=<?php echo $formToken?>"
         kr-public-key="demo:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5"
-        kr-company-name="Gonzales S.L."
+        kr-shop-name="Gonzales S.L."
         kr-button-label="pagar"
         kr-order-summary="1 protector de pantalla"
         kr-post-url="/paid.php"
